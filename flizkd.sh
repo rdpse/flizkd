1aca2b161134430a4e44229f3e117b40140255fb
mindfk <rduartenes@gmail.com>
2013-11-08 01:03:44 +0000

    chanegs 2

--- flizkd.sh
+++ flizkd.sh
@@ -13,8 +13,8 @@
 #    exit 0
 #fi
 
-distro=$(lsb_release -ds | awk '{ printf $0 }')
-os_version=$(lsb_release -rs | awk '{ printf $0 }')
+distro=$(lsb_release -ds | awk '{ printf $2" "$3" "$4 }')
+os_version=$(lsb_release -ds | awk '{ printf $3 }')
 arch=$(uname -m)
 kscheck=$(hostname | cut -d. -f2)
 IP=$(ifconfig eth0 | grep 'inet addr' | awk -F: '{ printf $2 }' | awk '{ printf $1 }')
@@ -96,7 +96,7 @@ case $os_version in
         ub1011=no
         ub1011x=no
         ;;
-        7.[0-9])
+        7)
         var9=yes
         debian=yes
         deb7=yes
